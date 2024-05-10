03e09652a21f7e4e004ae6b19a7da098c52186b9
Utkarsh1012Dewan <108482043+Utkarsh1012Dewan@users.noreply.github.com>
2024-05-10 17:00:54 +0530

    Added the genre tray. Fixed the grid display

---
+++ src/Components/GenreList.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useGenres } from '../Hooks/useGenres';
+const GenreList = () => {
+
+    const { genres } = useGenres();
+
+    return (
+        <ul>
+            {genres.map(g => <li key={g.id}>{g.name}</li>)}
+        </ul>
+    )
+}
+
+export default GenreList
