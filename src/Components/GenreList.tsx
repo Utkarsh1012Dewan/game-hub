4b7346c0852f2f0d00ad260c4dcedf1efe33a25c
Utkarsh1012Dewan <108482043+Utkarsh1012Dewan@users.noreply.github.com>
2024-05-13 16:29:58 +0530

    Added genre images and fixed the layout issue

--- src/Components/GenreList.tsx
+++ src/Components/GenreList.tsx
@@ -1,12 +1,19 @@
+import { HStack, Image, List, ListItem, Text } from '@chakra-ui/react';
 import { useGenres } from '../Hooks/useGenres';
+import getCroppedImageUrl from '../Services/image-url';
 const GenreList = () => {
 
     const { data } = useGenres();
 
     return (
-        <ul>
-            {data.map(g => <li key={g.id}>{g.name}</li>)}
-        </ul>
+        <List>
+            {data.map((g) => <ListItem key={g.id} padding='5px'>
+                <HStack>
+                    <Image boxSize='32px' borderRadius={8} src={getCroppedImageUrl(g.image_background)} />
+                    <Text fontSize='lg'>{g.name}</Text>
+                </HStack>
+            </ListItem>)}
+        </List>
     )
 }
 
